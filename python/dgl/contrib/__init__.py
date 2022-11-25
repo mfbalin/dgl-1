a61a537ad1830848656f4d2c88981924b196d2c5
xiny <xiny@nvidia.com>
2022-11-25 16:03:38 +0800

    remove GPUCachedTensor

--- python/dgl/contrib/__init__.py
+++ python/dgl/contrib/__init__.py
@@ -3,4 +3,5 @@ from . import graph_store
 from . import dist_sampling 
 from .dis_kvstore import KVClient, KVServer
 from .dis_kvstore import read_ip_config
+from .gpu_cache import GPUCache
 from .unified_tensor import UnifiedTensor
