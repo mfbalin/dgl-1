2f58c53d4fe3eda7e8d7f33e17aa5c5694b6729a
Muhammed Fatih Balin <m.f.balin@gmail.com>
2023-03-18 00:10:03 +0000

    revert removal of mmap

--- examples/pytorch/graphsage/load_graph.py
+++ examples/pytorch/graphsage/load_graph.py
@@ -50,7 +50,7 @@ def load_mag240m(root="dataset"):
     paper_offset = dataset.num_authors + dataset.num_institutions
     num_nodes = paper_offset + dataset.num_papers
     num_features = dataset.num_paper_features
-    feats = th.tensor(np.memmap(
+    feats = th.from_numpy(np.memmap(
         join(root, 'mag240m_kddcup2021/full.npy'),
         mode="r",
         dtype="float16",
