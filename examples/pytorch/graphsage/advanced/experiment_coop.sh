fa573bbb6e52c65e3755e1b16c9e8876a9dab450
Muhammed Fatih Balin <m.f.balin@gmail.com>
2023-05-17 20:49:57 +0000

    push experimental script changes

--- examples/pytorch/graphsage/advanced/experiment_coop.sh
+++ examples/pytorch/graphsage/advanced/experiment_coop.sh
@@ -1,5 +1,5 @@
 for dataset in ogbn-products ogbn-papers100M ogbn-mag240M; do
-for batch_size in 1024; do
+for batch_size in 512; do
 
 case "${dataset}" in
 ogbn-papers100M) varg=1;;
@@ -8,7 +8,7 @@ esac
 
 case "${dataset}" in
 ogbn-mag240M)   part=random;;
-*)              part=metis;;
+*)              part=random;;
 esac
 
 for replication in 0; do
@@ -16,7 +16,7 @@ for replication in 0; do
 model=rgcn
 multiplier=512
 
-for cache_size in 0 1000000; do
+for cache_size in 0 250000; do
 for sampler in labor neighbor; do
 for kappa in 1 256; do
 
@@ -27,7 +27,7 @@ case "${cache_size}" in
 *)  unset vvarg;;
 esac
 
-torchrun --nnodes=1:64 --nproc_per_node=1 --rdzv_id=123123123 --rdzv_backend=c10d --rdzv_endpoint=127.0.0.1:25555 train_dist_coop.py --num-epochs=10 --dataset=${dataset} --batch-size=${batch_size} --num-hidden=1024 --uva-ndata=features --cache-size=${cache_size} --sampler=${sampler} --batch-dependency=${kappa} --replication=${replication} --logdir=tb_logs_runtimes_coop_${replication} --model=${model} --partition=${part} --num-parts-multiplier=${multiplier} ${varg:+--undirected} ${vvarg:+--uva-data}
+torchrun --nnodes=1:64 --nproc_per_node=1 --rdzv_id=123123123 --rdzv_backend=c10d --rdzv_endpoint=127.0.0.1:25555 train_dist_coop.py --num-epochs=20 --dataset=${dataset} --batch-size=${batch_size} --num-hidden=1024 --uva-ndata=features --cache-size=${cache_size} --sampler=${sampler} --batch-dependency=${kappa} --replication=${replication} --logdir=/localscratch/tb_logs_runtimes_coop_${replication} --model=${model} --partition=${part} --num-parts-multiplier=${multiplier} ${varg:+--undirected} ${vvarg:+--uva-data}
 
 fi
 
